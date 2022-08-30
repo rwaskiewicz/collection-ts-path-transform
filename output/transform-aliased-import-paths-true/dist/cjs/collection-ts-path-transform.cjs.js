51c913c8bbabd790e3172fccd5826f627c3d612b
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2022-08-30 16:22:45 -0400

    add output of build step to `output/transform-alised-import-paths-true`

---
+++ output/transform-aliased-import-paths-true/dist/cjs/collection-ts-path-transform.cjs.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const index = require('./index-8ef4c5a0.js');
+
+/*
+ Stencil Client Patch Browser v0.0.0-dev.20220830190520 | MIT Licensed | https://stenciljs.com
+ */
+const patchBrowser = () => {
+    const importMeta = (typeof document === 'undefined' ? new (require('u' + 'rl').URL)('file:' + __filename).href : (document.currentScript && document.currentScript.src || new URL('collection-ts-path-transform.cjs.js', document.baseURI).href));
+    const opts = {};
+    if (importMeta !== '') {
+        opts.resourcesUrl = new URL('.', importMeta).href;
+    }
+    return index.promiseResolve(opts);
+};
+
+patchBrowser().then(options => {
+  return index.bootstrapLazy([["my-component.cjs",[[1,"my-component",{"first":[1],"middle":[1],"last":[1]}]]]], options);
+});
---
+++ output/transform-aliased-import-paths-true/dist/esm/collection-ts-path-transform.js
@@ -0,0 +1,17 @@
+import { p as promiseResolve, b as bootstrapLazy } from './index-216b2e1c.js';
+
+/*
+ Stencil Client Patch Browser v0.0.0-dev.20220830190520 | MIT Licensed | https://stenciljs.com
+ */
+const patchBrowser = () => {
+    const importMeta = import.meta.url;
+    const opts = {};
+    if (importMeta !== '') {
+        opts.resourcesUrl = new URL('.', importMeta).href;
+    }
+    return promiseResolve(opts);
+};
+
+patchBrowser().then(options => {
+  return bootstrapLazy([["my-component",[[1,"my-component",{"first":[1],"middle":[1],"last":[1]}]]]], options);
+});
